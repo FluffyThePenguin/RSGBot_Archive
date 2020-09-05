4ff009342fa079f1016c87a7aa60c5bce9962dd7
JeremyTCD <jeremytanchongdao@gmail.com>
2020-09-05 19:14:07 +0800

    Updated ExampleFeature.

--- src/features/exampleFeature/ExampleFeature.ts
+++ src/features/exampleFeature/ExampleFeature.ts
@@ -14,9 +14,9 @@ export default class ExampleFeature extends Feature {
     }
 
     /**
-     * Logs comment and replies if it contains the substring "hello".
+     * Logs comment, replies if it contains "hello".
      */
-    protected async onComment(comment: Comment, _: Command): Promise<void> {
+    public async onComment(comment: Comment, _: Command): Promise<void> {
         this._logger.info(`onComment, author: ${comment.author.name}, comment body: ${comment.body}`);
 
         if (comment.body.toLowerCase().includes('hello')) {
@@ -26,21 +26,21 @@ export default class ExampleFeature extends Feature {
     }
 
     /**
-     * Logs submission and replies if its title contains the substring "hello".
+     * Logs submission, replies if its title contains "hello".
      */
-    protected async onSubmission(submission: Submission): Promise<void> {
+    public async onSubmission(submission: Submission): Promise<void> {
         this._logger.info(`onSubmission, author: ${submission.author.name}, submission title: ${submission.title}`);
 
         if (submission.title.toLowerCase().includes('hello')) {
             //@ts-ignore
-            await submission.reply(`echo: ${submission.title}`);
+            await submission.reply(`Hi!`);
         }
     }
 
     /**
-     * Logs private message and replies it with a copy of its body.
+     * Logs private message, replies with a copy of its body.
      */
-    protected async onPrivateMessage(privateMessage: PrivateMessage, _: Command): Promise<void> {
+    public async onPrivateMessage(privateMessage: PrivateMessage, _: Command): Promise<void> {
         this._logger.info(`onPrivateMessage, author: ${privateMessage.author.name}, private message body: ${privateMessage.body}`);
 
         //@ts-ignore
@@ -50,7 +50,7 @@ export default class ExampleFeature extends Feature {
     /**
      * Logs onInit event.
      */
-    protected async onInit(): Promise<void> {
+    public async onInit(): Promise<void> {
         this._logger.info('onInit');
     }
 }
--- test/features/exampleFeature/ExampleFeature.test.ts
+++ test/features/exampleFeature/ExampleFeature.test.ts
@@ -1,33 +1,33 @@
 import "reflect-metadata";
 import { mocked } from "ts-jest/utils";
 import ExampleFeature from "../../../src/features/exampleFeature/ExampleFeature";
-import Logger from "../../../src/shared/Logger";
+import Logger from "../../../src/shared/logging/Logger";
 import Comment from "snoowrap/dist/objects/Comment";
 import RedditUser from "snoowrap/dist/objects/RedditUser";
 import Submission from "snoowrap/dist/objects/Submission";
 import PrivateMessage from "snoowrap/dist/objects/PrivateMessage";
 
-jest.mock("../../../src/shared/Logger");
+jest.mock("../../../src/shared/logging/Logger");
 jest.mock("snoowrap/dist/objects/Comment");
 jest.mock("snoowrap/dist/objects/RedditUser");
 jest.mock("snoowrap/dist/objects/Submission");
 jest.mock("snoowrap/dist/objects/PrivateMessage");
 
 describe('onComment', () => {
-    test('logs comment details and replies', async () => {
-        // Arrange
-        const dummyAuthorName = 'dummyAuthorName';
-        const dummyBody = 'dummyBody';
+    const dummyAuthorName = 'dummyAuthorName';
+    const dummyBody = 'dummyBody';
+    const dummyBodyWithHello = 'dummyBody hello';
 
+    test('logs comment details', async () => {
+        // Arrange
         const mockRedditUser = new RedditUser(null, null, null);
         mockRedditUser.name = dummyAuthorName;
-        
+
         const mockComment = new Comment(null, null, null);
         mockComment.author = mockRedditUser;
         mockComment.body = dummyBody;
 
-        const mockLogger = new Logger();
-
+        const mockLogger = new Logger(null);
         const testSubject = new ExampleFeature(mockLogger);
 
         // Act
@@ -35,28 +35,57 @@ describe('onComment', () => {
 
         // Assert
         const mockLoggerTyped = mocked(mockLogger);
-        expect(mockLoggerTyped.log.mock.calls).toEqual([['ExampleFeature', `onComment, author: ${dummyAuthorName}, comment body: ${dummyBody}`]]);
+        expect(mockLoggerTyped.info.mock.calls[0][0]).toEqual(`onComment, author: ${dummyAuthorName}, comment body: ${dummyBody}`);
+    });
 
+    test('replies comment if it contains "hello"', async () => {
+        // Arrange
+        const mockComment = new Comment(null, null, null);
+        mockComment.author = new RedditUser(null, null, null);
+        mockComment.body = dummyBodyWithHello;
+
+        const testSubject = new ExampleFeature(new Logger(null));
+
+        // Act
+        await testSubject.onComment(mockComment, null);
+
+        // Assert
         const mockCommentTyped = mocked(mockComment);
-        expect(mockCommentTyped.reply.mock.calls).toEqual([[`echo: ${dummyBody}`]]);
+        expect(mockCommentTyped.reply.mock.calls[0][0]).toEqual('Hi!');
+    });
+
+    test('ignores comment if it does not contain "hello"', async () => {
+        // Arrange
+        const mockComment = new Comment(null, null, null);
+        mockComment.author = new RedditUser(null, null, null);
+        mockComment.body = dummyBody;
+
+        const testSubject = new ExampleFeature(new Logger(null));
+
+        // Act
+        await testSubject.onComment(mockComment, null);
+
+        // Assert
+        const mockCommentTyped = mocked(mockComment);
+        expect(mockCommentTyped.reply.mock.calls.length).toEqual(0);
     });
 });
 
 describe('onSubmission', () => {
-    test('logs submission details and replies', async () => {
+    const dummyAuthorName = 'dummyAuthorName';
+    const dummyTitle = 'dummyTitle';
+    const dummyTitleWithHello = 'dummyTitle hello';
+
+    test('logs submission details', async () => {
         // Arrange
-        const dummyAuthorName = 'dummyAuthorName';
-        const dummyTitle = 'dummyTitle';
-        
         const mockRedditUser = new RedditUser(null, null, null);
         mockRedditUser.name = dummyAuthorName;
-        
+
         const mockSubmission = new Submission(null, null, null);
         mockSubmission.author = mockRedditUser;
         mockSubmission.title = dummyTitle;
 
-        const mockLogger = new Logger();
-        
+        const mockLogger = new Logger(null);
         const testSubject = new ExampleFeature(mockLogger);
 
         // Act
@@ -64,10 +93,39 @@ describe('onSubmission', () => {
 
         // Assert
         const mockLoggerTyped = mocked(mockLogger);
-        expect(mockLoggerTyped.log.mock.calls).toEqual([['ExampleFeature', `onSubmission, author: ${dummyAuthorName}, submission title: ${dummyTitle}`]]);
+        expect(mockLoggerTyped.info.mock.calls[0][0]).toEqual(`onSubmission, author: ${dummyAuthorName}, submission title: ${dummyTitle}`);
+    });
 
-        const mockCommentTyped = mocked(mockSubmission);
-        expect(mockCommentTyped.reply.mock.calls).toEqual([[`echo: ${dummyTitle}`]]);
+    test('replies submission if its title contains "hello"', async () => {
+        // Arrange
+        const mockSubmission = new Submission(null, null, null);
+        mockSubmission.author = new RedditUser(null, null, null);
+        mockSubmission.title = dummyTitleWithHello;
+
+        const testSubject = new ExampleFeature(new Logger(null));
+
+        // Act
+        await testSubject.onSubmission(mockSubmission);
+
+        // Assert
+        const mockSubmissionTyped = mocked(mockSubmission);
+        expect(mockSubmissionTyped.reply.mock.calls[0][0]).toEqual('Hi!');
+    });
+
+    test('ignores submission if its titles does not contain "hello"', async () => {
+        // Arrange
+        const mockSubmission = new Submission(null, null, null);
+        mockSubmission.author = new RedditUser(null, null, null);
+        mockSubmission.title = dummyTitle;
+
+        const testSubject = new ExampleFeature(new Logger(null));
+
+        // Act
+        await testSubject.onSubmission(mockSubmission);
+
+        // Assert
+        const mockSubmissionTyped = mocked(mockSubmission);
+        expect(mockSubmissionTyped.reply.mock.calls.length).toEqual(0);
     });
 });
 
@@ -76,16 +134,15 @@ describe('onPrivateMessage', () => {
         // Arrange
         const dummyAuthorName = 'dummyAuthorName';
         const dummyBody = 'dummyBody';
-        
+
         const mockRedditUser = new RedditUser(null, null, null);
         mockRedditUser.name = dummyAuthorName;
-        
+
         const mockPrivateMessage = new PrivateMessage(null, null, null);
         mockPrivateMessage.author = mockRedditUser;
         mockPrivateMessage.body = dummyBody;
 
-        const mockLogger = new Logger();
-        
+        const mockLogger = new Logger(null);
         const testSubject = new ExampleFeature(mockLogger);
 
         // Act
@@ -93,17 +150,17 @@ describe('onPrivateMessage', () => {
 
         // Assert
         const mockLoggerTyped = mocked(mockLogger);
-        expect(mockLoggerTyped.log.mock.calls).toEqual([['ExampleFeature', `onPrivateMessage, author: ${dummyAuthorName}, private message body: ${dummyBody}`]]);
+        expect(mockLoggerTyped.info.mock.calls[0][0]).toEqual(`onPrivateMessage, author: ${dummyAuthorName}, private message body: ${dummyBody}`);
 
-        const mockCommentTyped = mocked(mockPrivateMessage);
-        expect(mockCommentTyped.reply.mock.calls).toEqual([[`echo: ${dummyBody}`]]);
+        const mockPrivateMessageTyped = mocked(mockPrivateMessage);
+        expect(mockPrivateMessageTyped.reply.mock.calls[0][0]).toEqual(`echo: ${dummyBody}`);
     });
 });
 
 describe('onInit', () => {
     test('logs', async () => {
         // Arrange
-        const mockLogger = new Logger();
+        const mockLogger = new Logger(null);
         const testSubject = new ExampleFeature(mockLogger);
 
         // Act
@@ -111,6 +168,6 @@ describe('onInit', () => {
 
         // Assert
         const mockLoggerTyped = mocked(mockLogger);
-        expect(mockLoggerTyped.log.mock.calls).toEqual([['ExampleFeature', 'onInit']]);
+        expect(mockLoggerTyped.info.mock.calls[0][0]).toEqual('onInit');
     });
 });
