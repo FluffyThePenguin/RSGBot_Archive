a272d9733684f6ede98cc68a01f0624e6a87760e
JeremyTCD <jeremytanchongdao@gmail.com>
2020-10-03 21:41:01 +0800

    Initial command parser.

--- src/shared/commands/Command.ts
+++ src/shared/commands/Command.ts
@@ -1,6 +1,6 @@
-import { Key } from "readline";
 
 // Immutable since multiple features will receive the same command
+// Options may be null (a command may have no options)
 export default class Command {
     constructor(public readonly name: string, public readonly options: ReadonlyMap<string, string>) {}
 }
